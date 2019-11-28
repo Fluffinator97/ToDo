86812f7e6df45820bde555a327984783e544acaa
Claudia Aranda <claudia_15_@hotmail.com>
2019-11-28 16:26:12 +0100

    6.Skapa och designa kalendervyn med en månadsvy över aktuell månad - (G) Fixat✅

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const addItems = document.querySelector('.add-items')
 const itemsList = document.querySelector('.todo-list')
-const items = []
+const items = JSON.parse(localStorage.getItem('items')) || []
 
 function addItem(e) {
    e.preventDefault()
@@ -13,6 +13,7 @@ function addItem(e) {
        }
        items.push(item)
        populateList(items, itemsList)
+       localStorage.setItem('items', JSON.stringify(items))
        this.reset()
    }
 
@@ -27,5 +28,55 @@ function addItem(e) {
     }).join('')
    }
 
+function toggleDone(e){
+    console.log(e)
+}
+
 
 addItems.addEventListener('submit', addItem)
+itemsList.addEventListener('click', toggleDone)
+
+populateList(items,itemsList)
+
+function formatDate(date) {
+    let monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+  
+    let day = date.getDate();
+    let monthIndex = date.getMonth();
+    let year = date.getFullYear();
+  
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+  }
+  
+
+  document.getElementById('date').innerHTML = formatDate(new Date()) 
+
+  let currentDate = new Date()
+  let lastDayOfMonth = new Date(currentDate.getFullYear(), 0 + 1, 0)
+  let daysInCurrentMonth = lastDayOfMonth.getDate()
+  let dayInWeek = new Date(currentDate.getFullYear(), 0 + 1, 1).getDay()
+
+  const calenderWrapper = document.querySelector('.calenderWrapper')
+
+  console.log(dayInWeek)
+
+
+  for(let i = 1; i <= dayInWeek; i++) {
+      let dayCard = document.createElement('div')
+      // class="days"
+      dayCard.classList = 'days'
+      calenderWrapper.appendChild(dayCard)
+  }
+
+  for(let i = 1; i <= daysInCurrentMonth; i++) {
+      let dayCard = document.createElement('div')
+      dayCard.innerText = i
+      // class="days"
+      dayCard.classList = 'days'
+      calenderWrapper.appendChild(dayCard)
+  }
