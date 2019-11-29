13e80a3f72b0b5f362a5ddaf52ebaf9a5a17e526
Claudia Aranda <claudia_15_@hotmail.com>
2019-11-29 11:26:18 +0100

    ändringar i todo

--- script.js
+++ script.js
@@ -3,34 +3,61 @@ const addItems = document.querySelector('.add-items')
 const itemsList = document.querySelector('.todo-list')
 const items = JSON.parse(localStorage.getItem('items')) || []
 
-function addItem(e) {
-   e.preventDefault()
+function addItem(event) {
+   event.preventDefault()
   
        const text = (this.querySelector('[name=item]')).value
        const item = {
            text: text,
            done: false
        }
+  
        items.push(item)
        populateList(items, itemsList)
        localStorage.setItem('items', JSON.stringify(items))
        this.reset()
    }
 
+   
    function populateList(lists = [], todoList) {
     todoList.innerHTML = lists.map((list, i) => {
         return `
             <li> 
-                <input type="checkbox" data-index=${i} id="item${i}" ${list.done ? 'checked' : ' '} />
+                <input type="checkbox" data-index=${i} id="item${i}" ${list.done ? 'checked' : ' '}  />
                 <label for="">${list.text}</label>
             </li>
         `
     }).join('')
    }
-
-function toggleDone(e){
-    console.log(e)
+/**
+ * @typedef {index} index of data-index
+ * @param {event} event 
+ */
+function toggleDone(event){
+    if(!event.target.matches('input'))return
+    const el = event.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
 }
+
+
+
+
+/*KOLLA OM DETTA FUNKAR */
+function removeTodoListener(event) {
+    // Remove the li element from the DOM
+    const li = event.target.parentElement
+    li.remove()
+
+    // Also update local storage
+    const todoText = event.target.nextSibling.textContent
+    removeTodo(todoText)
+}
+/*KOLLA OM DETTA FUNKAR */
+
+
 
 
 addItems.addEventListener('submit', addItem)
@@ -57,9 +84,12 @@ function formatDate(date) {
   document.getElementById('date').innerHTML = formatDate(new Date()) 
 
   let currentDate = new Date()
+//   let currentMonth = today.getMonth();
   let lastDayOfMonth = new Date(currentDate.getFullYear(), 0 + 1, 0)
   let daysInCurrentMonth = lastDayOfMonth.getDate()
   let dayInWeek = new Date(currentDate.getFullYear(), 0 + 1, 1).getDay()
+
+//   let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   const calenderWrapper = document.querySelector('.calenderWrapper')
 
@@ -70,7 +100,7 @@ function formatDate(date) {
       let dayCard = document.createElement('div')
       // class="days"
       dayCard.classList = 'days'
-      calenderWrapper.appendChild(dayCard)
+      calenderWrapper.appendChild(dayCard)         
   }
 
   for(let i = 1; i <= daysInCurrentMonth; i++) {
@@ -80,3 +110,5 @@ function formatDate(date) {
       dayCard.classList = 'days'
       calenderWrapper.appendChild(dayCard)
   }
+
+ 
