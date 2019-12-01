aa7238eb5f86725f2e50776a4d323a5dd55d8f2b
Claudia Aranda <claudia_15_@hotmail.com>
2019-12-02 00:27:35 +0100

    fixed current month showing as a heder (needs css fix)

--- calendar.js
+++ calendar.js
@@ -1,52 +1,75 @@
-let monthNames = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
-  ];
+let allMonths = [
+	"Januari",
+	"Februari",
+	"Mars",
+	"April",
+	"Maj",
+	"Juni",
+	"Juli",
+	"Augusti",
+	"September",
+	"Oktober",
+	"November",
+	"December"
+]
+
+ 
   
-  function getFullDate(date) {
-      let day = date.getDate()
-      let monthIndex = date.getMonth()
-      let year = date.getFullYear()
+  function getFullDate(today) {
+     // let today = new Date()
+      let day = today.getDate()
+      let monthNames = today.getMonth();
+      let year = today.getFullYear()
     
       return `
-      ${day} ${ monthNames[monthIndex]} ${year}
+      ${day} ${ monthNames[allMonths]} ${year}
       `
   }
-  document.getElementById('date').innerHTML = getFullDate(new Date()) 
-  
-  
-//   function month(monthNames){
-//       let monthIndex = monthNames.getMonth()
-//       return `
-//       ${ monthNames[monthIndex]}
-//       `
-//   }   
+    document.createElement('date').innerHTML = getFullDate(new Date()) 
    
-//     document.getElementById('h1').innerHTML = month(new Date() )
   
-
-
-    let currentDate = new Date()
-    let currentMonth = currentDate.getMonth();
-    let lastDayOfMonth = new Date(currentDate.getFullYear(), currentMonth + 1, 0)
+//   function thisMonth(monthNames){
+//       let month = date.getMonth()
+     
+//       return `
+//       ${ monthNames[month]}
+//       `
+//   }    
+//     document.querySelector('.month').innerHTML = thisMonth(new Date() )
     
+
+
+    let today = new Date()
+    let currentMonth = today.getMonth();
+    let lastDayOfMonth = new Date(today.getFullYear(), currentMonth + 1, 0)
+    let year = today.getFullYear();
     let daysInCurrentMonth = lastDayOfMonth.getDate()
-    let dayInWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay()
+    let dayInWeek = new Date(today.getFullYear(), today.getMonth(), 1).getDay()
     
     const calenderWrapper = document.querySelector('.calenderWrapper')
+    const header = document.querySelector('header')
+
+   
     
     console.log("Selected month", currentMonth)
     console.log("Day in week: ", dayInWeek)
-  
+    /**
+    * Asynchronous `for` loop.
+    * @function for
+    * @param {function} initial Sets up initial condition for loop. i = 1 (adding one day causes the date to roll over to the next month is necessary)
+    * @param {function} condition Checksif index is bigger then 6
+    * @param {function} update add 1 after exicution
+    * @returns {Promise} Resolves if successful; otherwise rejects with error.
+    */
     //renders empty boxes in calender
+function emtyDates(dayInWeek){
     for(let i = 1; i < dayInWeek; i++) {
         let dayCard = document.createElement('div')
         dayCard.classList = 'days'
         calenderWrapper.appendChild(dayCard)         
     }
-  //
+}
+  //shows dates in calender cards
     for(let i = 1; i <= daysInCurrentMonth; i++) {
         let dayCard = document.createElement('div')
         dayCard.innerText = i
@@ -54,6 +77,20 @@ let monthNames = [
         dayCard.classList = 'days'
         calenderWrapper.appendChild(dayCard)
     }
+  //shows current month in header
+    for(let i = 0; i <= daysInCurrentMonth; i++) {
+        if(i === currentMonth){
+            let month = document.createElement('h1')
+            month.innerText = allMonths[i]
+            month.classList = 'curentMonthYear'
+            header.appendChild(month)
+        }
+        
+    }
+
+    // function daysInMonth(currentMonth, year) {
+    //     // day 0 here returns the last day of the PREVIOUS month
+    //     return new Date(year, month + 1, 0).getDate();
+    // }
   
-   
-  
+ 
--- script.js
+++ script.js
@@ -64,3 +64,48 @@ itemsList.addEventListener('click', toggleDone)
 
 populateList(items,itemsList)
 
+// let monthNames = [
+//   "January", "February", "March",
+//   "April", "May", "June", "July",
+//   "August", "September", "October",
+//   "November", "December"
+// ];
+
+// function getFullDate(date) {
+//     let day = date.getDate();
+//     let monthIndex = date.getMonth();
+//     let year = date.getFullYear();
+  
+//     return day + ' ' + monthNames[monthIndex] + ' ' + year;
+// }
+  
+//   document.getElementById('date').innerHTML = formatDate(new Date()) 
+
+//   let currentDate = new Date()
+//   let currentMonth = currentDate.getMonth();
+//   let lastDayOfMonth = new Date(currentDate.getFullYear(), currentMonth + 1, 0)
+  
+//   let daysInCurrentMonth = lastDayOfMonth.getDate()
+//   let dayInWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay()
+  
+//   const calenderWrapper = document.querySelector('.calenderWrapper')
+  
+//   console.log("Selected month", currentMonth)
+//   console.log("Day in week: ", dayInWeek)
+
+//   //renders empty boxes in calender
+//   for(let i = 1; i < dayInWeek; i++) {
+//       let dayCard = document.createElement('div')
+//       dayCard.classList = 'days'
+//       calenderWrapper.appendChild(dayCard)         
+//   }
+// //
+//   for(let i = 1; i <= daysInCurrentMonth; i++) {
+//       let dayCard = document.createElement('div')
+//       dayCard.innerText = i
+//       // class="days"
+//       dayCard.classList = 'days'
+//       calenderWrapper.appendChild(dayCard)
+//   }
+
+ 
