13ad58e9e981fa7af348e0b8377d985ad0250cfe
Claudia Aranda <claudia_15_@hotmail.com>
2019-12-05 14:05:10 +0100

    fixed so calender can change month + year + start from the right weedkday in calender

--- calendar.js
+++ calendar.js
@@ -14,27 +14,34 @@ let allMonths = [
 ]
 
 let today = new Date()
-let thisDay = today.getDate();
-let currentMonth = today.getMonth();
+let thisDay = today.getDate()
+let currentMonth = today.getMonth()
 let lastDayOfMonth = new Date(today.getFullYear(), currentMonth + 1, 0)
-let year = today.getFullYear();
+console.log("date : ", lastDayOfMonth)
+console.log("sueernt month: ", currentMonth)
+let year = today.getFullYear()
 let daysInCurrentMonth = lastDayOfMonth.getDate()
-let startingWeekDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDay()
+let startingWeekDayOfMonth = new Date(today.getFullYear(), currentMonth, 0).getDay()
 
 const calenderWrapper = document.querySelector('.calenderWrapper')
-const header = document.querySelector('header')
+const monthInHeader = document.querySelector('header')
+// const yearsInHeader = document.querySelector('p')
 
 
 window.addEventListener('load', createCalendar)
 
 
 function createCalendar() {
+    console.log(currentMonth)
+    calenderWrapper.innerHTML = ''
+    monthInHeader.innerHTML = ''
     createEmptyDateCards()
     createCalenderCards()
     showCurrentMonthInHeader()
 }
 
-function createEmptyDateCards() {   
+function createEmptyDateCards() {
+    startingWeekDayOfMonth = new Date(today.getFullYear(), currentMonth, 0).getDay()
     for (let i = 0; i < startingWeekDayOfMonth; i++) {
         let dayCard = document.createElement('div')
         dayCard.classList = 'days'
@@ -43,6 +50,9 @@ function createEmptyDateCards() {
 }
 
 function createCalenderCards() {
+    lastDayOfMonth = new Date(today.getFullYear(), currentMonth + 1, 0)
+    daysInCurrentMonth = lastDayOfMonth.getDate()
+    console.log("daysInCurrentMonth: ", daysInCurrentMonth)
     for (let i = 1; i <= daysInCurrentMonth; i++) {
         let dayCard = document.createElement('div')
         dayCard.innerText = i
@@ -52,41 +62,63 @@ function createCalenderCards() {
         let innerDiv = document.createElement('div')
         innerDiv.className = 'toDoCard'
         dayCard.appendChild(innerDiv)
-        // if (
-	    //     thisDay === today &&
-		// 	currentMonth === today.getMonth() &&
-		// 	year === today.getFullYear()
-		// ) {
-		// 	dayCard.classList = 'active'
-		// }
+       
     }
 }
 
+
+function nextMonth() {
+    if(currentMonth === 11){
+        year = year +1
+    }else{
+        year = year
+    }
+
+    if(currentMonth === 11){
+        currentMonth = 0
+    }else{
+        currentMonth = currentMonth +1
+    }
+   createCalendar()
+}
+document.getElementById('next').addEventListener("click", nextMonth)
+
+function prevMonth() {
+    if(currentMonth === 0){
+        year = year-1
+    } else{
+        year = year
+    }
+
+    if(currentMonth === 0){
+        currentMonth = 11
+    } else{
+        currentMonth = currentMonth-1
+    }
+    createCalendar(currentMonth)
+}
+document.getElementById('prev').addEventListener("click", prevMonth)
 
 
 
 function showCurrentMonthInHeader() {
     for (let i = 0; i <= daysInCurrentMonth; i++) {
-        if (i === currentMonth) {
+        if (i === (currentMonth)) {
             let month = document.createElement('h1')
+            let thisyear = document.createElement('h1')
             month.innerText = allMonths[i]
             month.classList = 'curentMonthYear'
-            header.appendChild(month)
+            monthInHeader.appendChild(month)
+            thisyear.innerText = year
+            thisyear.classList = 'curentMonthYear'
+            monthInHeader.appendChild(thisyear)
+
         }
+        
     }
 }
 
-let nextMonthBtn = document.getElementById('next').addEventListener("click", nextMonth)
-let prevMonthBtn = document.getElementById('prev').addEventListener("click", prevMonth)
 
-function nextMonth(){
-    year = currentMonth === 11 ? year + 1 : year
-	currentMonth = (currentMonth + 1) % 12
-	createCalendar(currentMonth, year)
-}
 
-function prevMonth(){
-    year = currentMonth === 0 ? year - 1 : year
-	currentMonth = currentMonth === 0 ? 11 : currentMonth - 1
-	createCalendar(currentMonth, year)
-}
+
+
