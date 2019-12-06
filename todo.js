62e4a9831923e5b216aa5759d5b9ca2ae80995d5
Kristóf <krstf456@gmail.com>
2019-12-06 20:19:40 +0100

    header blev mindre och några små ändringar

--- todo.js
+++ todo.js
@@ -64,8 +64,9 @@ function addNewToDo(){
     let div = this.parentElement
     div.style.display = "none"
     let re = new RegExp('[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])');
-    let todoremoveItem = div.innerText.match(re)
-   // console.log(todoremoveItem[0])
+    let todoRemoveItem = div.innerText.match(re)
+    // console.log(todoremoveItem[0])
     localStorage.removeItem(key)
+    
   }
 }
